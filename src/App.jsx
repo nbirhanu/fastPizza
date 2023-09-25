580a049d2af1392bfc56a423aeab850c491cde3c
nbirhanu <nbirhanu80@gmail.com>
2023-09-25 23:09:21 +0300

    fastPizza

---
+++ src/App.jsx
@@ -0,0 +1,41 @@
+import { RouterProvider, createBrowserRouter } from 'react-router-dom'
+import Home from './ui/Home'
+import Menu, { menuLoader } from './features/menu/Menu'
+import Cart from './features/cart/Cart'
+import CreateOrder from './features/order/CreateOrder'
+import Order from './features/order/Order'
+import AppLayout from './ui/AppLayout'
+const router = createBrowserRouter([
+	{
+		element: <AppLayout />,
+		children: [
+			{
+				path: '/',
+				element: <Home />,
+			},
+			{
+				path: '/menu',
+				element: <Menu />,
+				loader: menuLoader,
+			},
+			{
+				path: '/cart',
+				element: <Cart />,
+			},
+			{
+				path: '/order/new',
+				element: <CreateOrder />,
+			},
+			{
+				path: '/order/:Id',
+				element: <Order />,
+			},
+		],
+	},
+])
+
+const App = () => {
+	return <RouterProvider router={router}></RouterProvider>
+}
+
+export default App
